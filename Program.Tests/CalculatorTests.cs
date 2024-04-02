5fad3044a9f8bb391d7fbf1ffe8f374ff88e1abc
imkoolya <imkoolya@gmail.com>
2024-04-02 19:05:30 +0300

    16.7.1. Практика (HW-03)

---
+++ Program.Tests/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class CalculatorTests
+{
+    [Test]
+    public void Additional_MustReturnCorrectValue()
+    {
+        Calculator calculator = new Calculator();
+        Assert.That(calculator.Additional(300, 300) == 600);
+    }
+    [Test]
+    public void Subtraction_MustReturnCorrectValue()
+    {
+        Calculator calculator = new Calculator();
+        Assert.That(calculator.Subtraction(300, 10) == 290);
+    }
+    [Test]
+    public void Miltiplication_MustReturnCorrectValue()
+    {
+        Calculator calculator = new Calculator();
+        Assert.That(calculator.Miltiplication(300, 10) == 3000);
+    }
+    [Test]
+    public void Division_MustReturnCorrectValue()
+    {
+        Calculator calculator = new Calculator();
+        Assert.That(calculator.Division(300, 10) == 30);
+    }
+}
--- module-16/Program.cs
+++ module-16/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿public class Calculator
+{
+    public int Additional(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Miltiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Division(int a, int b)
+    {
+        return a / b;
+    }
+}
